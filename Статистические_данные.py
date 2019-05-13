d3f488dbb3279a24628da3153f4991ea18b696fe
Sonnik73 <Sonnik73@gmail.com>
2019-05-13 20:05:34 +0400

    Config update

--- Статистические_данные.py
+++ Статистические_данные.py
@@ -1,15 +1,23 @@
 # Import `load_workbook` module from `openpyxl`
 import os
 from openpyxl import load_workbook
+import io
+import re
 
 
+pattern = re.compile(r'\w+')
 PathData = ('./Data/')
 ListSheet = []
 PathFiles = os.listdir(path=PathData)
 PathToFile = "String"
-#NameSheet
+StrRead = "a"
+a="String"
+ValueVariable=0
+n=3
+Dict = {}
 
-print(PathData)
+
+#print(PathData)
 
 
 
@@ -19,11 +27,52 @@ class PathDocs(object):
  
     def __init__():
         """Constructor"""
+
+    def ConfigChecker():
+        with io.open('./Sys/Data.txt', encoding='utf-8') as file:
+            for line in file:
+                x = re.sub("^\s+|\n|\r|\s+$", '', line)
+
+                Data = open('config.txt', 'r')
+                lineS = Data.readline()
+                while lineS:
+                    #print(locals())
+                    if((len(pattern.findall(lineS)))!=0):
+                        if((len(pattern.findall(lineS)))==2):
+                            #print(str(pattern.findall(lineS)[0]) +" = "+ str(pattern.findall(lineS)[1]))
+                            #exec("%s = %d" % ((str(pattern.findall(lineS)[0])),float(str(pattern.findall(lineS)[1]))))
+                            NameVar = str(pattern.findall(lineS)[0])
+                            ValueVar = (float(str(pattern.findall(lineS)[1])))
+
+                            Dict.update({NameVar: ValueVar})
+
+                            #Dict[NameVar] = ValueVar
+                            lineS = Data.readline()
+
+                        if((len(pattern.findall(lineS)))==3):
+                            #print(str(pattern.findall(lineS)[0]) +" = "+ str(pattern.findall(lineS)[1]) +"."+ str(pattern.findall(lineS)[2]))
+                            NameVar = str(pattern.findall(lineS)[0])
+                            ValueVar = (float(str(pattern.findall(lineS)[1])+"."+str(pattern.findall(lineS)[2])))
+                            Dict.update({NameVar: ValueVar})
+                            #Dict[NameVar] = ValueVar
+                            #exec("%s = %d" % (NameVar,ValueVar))
+                            #print(str(NameVar)+" = " + str(ValueVar))
+                            lineS = Data.readline()
+
+                    else:
+                        #print(SugarMax)
+                        break
+                break
+
+
+    
     def CheckListFiles():
+        print("Начата проверка списка файлов в папке /Data/...")
         i=0
         for item in PathFiles:
             print(str(i+1)+"."+item)
             i=+1
+        print("Проверка списка файлов в папке /Data/ успешно завершена...")
     def OpenFile():
         i=0
         FileName = PathFiles[int(input("Какой фаил открыть?\n"))-1]
@@ -39,15 +88,15 @@ class PathDocs(object):
             ListSheet.append(sheet)
             i+=1
         sh = wb.active  
+
     def PrintData(row1,column1):
         print(ListSheet[0].cell(row=row1, column=column1).value)
 
+   
 
 
-
-
-
-
+PathDocs.ConfigChecker()
+print(Dict.items())
 PathDocs.CheckListFiles()
 PathToFile = PathDocs.OpenFile()
 PathDocs.SheetList()
