dfe23c1118c32294edc15e078bf80179aeb14d60
Sonnik73 <Sonnik73@gmail.com>
2019-05-27 20:37:48 +0400

    NeurolTest

--- Статистические_данные.py
+++ Статистические_данные.py
@@ -5,6 +5,7 @@ import io
 import re
 from pprint import pprint
 import sys
+from numpy import exp, array, random, dot
 
 
 pattern = re.compile(r'\w+')
@@ -92,14 +93,10 @@ class PathDocs(object):
             #print(str(i+1)+"."+str(sheet.title))
             ListSheet.append(sheet)
             i+=1
-
-        #sh = wb.active  
-
     def PrintData(row1,column1):
         Data = ListSheet[0].cell(row=row1, column=column1).value
         #print(Data)
         return(Data)
-
     def CalcDifference():
         PathDocs.PrintData(3,1)
         Data = 1
@@ -183,24 +180,13 @@ class PathDocs(object):
                             NowData=NowData.replace(' ', '')
                             NowData=float(NowData.strip())
                             if (str(pattern.findall(x)[3])==str(MinCountColumn)):
-                                if Max in x:
-                                    Calc=round((float(NowData)-float((pattern.findall(x)[1])+"."+(pattern.findall(x)[2]))),1)
-                                    if (Calc>=0):
-                                        print(pattern.findall(x)[0].replace('Max', '')+" выше нормы на "+ str(Calc)+" у № "+str(PathDocs.PrintData(int(MinCountRow),1)))
-                                        DataList=(DataList+" "+str(Calc))
-                                        #print(DataList)
-                                        MinCountColumn=MinCountColumn + 1
-                                        continue
-                                    else:
-                                        print(pattern.findall(x)[0].replace('Max', '')+" в пределах нормы "+ str(NowData)+" у № "+str(PathDocs.PrintData(int(MinCountRow),1)))
-                                        DataList=(DataList+" 0")
 
-
-                                elif Min in x:
+                                if Min in x:
                                     Calc=round((float((pattern.findall(x)[1])+"."+(pattern.findall(x)[2]))-float(NowData)),1)
                                     if (Calc>=0):
                                         print((pattern.findall(x)[0]).replace('Min', '')+" ниже нормы на "+ str(Calc)+" у № "+str(PathDocs.PrintData(int(MinCountRow),1)))
-                                        DataList=(DataList+" "+str(Calc))
+                                        #DataList=(DataList+" "+str(Calc))
+                                        DataList=(DataList+" "+str("1"))
                                         MinCountColumn=MinCountColumn + 1
                                         continue
                                     elif(Calc<=0):
@@ -219,12 +205,14 @@ class PathDocs(object):
                                                     if (Calc>=0):
                                                         print(str(pattern.findall(x1)[0])+" в таблице = "+str(NowData)+"а в конфиге = "+ (pattern.findall(x1)[1])+"."+(pattern.findall(x1)[2]))
                                                         print(pattern.findall(x1)[0].replace('Max', '')+" вSше нормы на "+ str(Calc)+" у № "+str(PathDocs.PrintData(int(MinCountRow),1)))
-                                                        DataList=(DataList+" "+str(Calc))
+                                                        #DataList=(DataList+" "+str(Calc))
+                                                        DataList=(DataList+" "+str(1))
                                                         MinCountColumn=MinCountColumn + 1
                                                         break
                                                     else:
                                                         print(pattern.findall(x)[0].replace('Max', '').replace('Min','')+" в норме при "+ str(NowData)+" у № "+str(PathDocs.PrintData(int(MinCountRow),1)))
-                                                        DataList=(DataList+" "+str(Calc))
+                                                        DataList=(DataList+" "+str(0))
+
                                                         MinCountColumn=MinCountColumn + 1
                                                         break
                                         continue
@@ -233,11 +221,14 @@ class PathDocs(object):
 
                                 else:
                                     Calc=round((float(NowData)-float((pattern.findall(x)[1])+"."+(pattern.findall(x)[2]))),1)
-                                    if(Calc>=0):
+                                    if(Calc<=0):
                                         Calc=round(float((pattern.findall(x)[1])+"."+(pattern.findall(x)[2]))-float(NowData),1)
-                                        
+                                        DataList=(DataList+" "+str(0))
+                                    else:
+                                        DataList=(DataList+" "+str(1))
                                     print(pattern.findall(x)[0]+" отличается на "+ str(Calc)+" у № "+str(PathDocs.PrintData(int(MinCountRow),1)))
-                                    DataList=(DataList+" "+str(Calc))
+                                    #DataList=(DataList+" "+str(Calc))
+                                    
                                     MinCountColumn=MinCountColumn + 1
                                     continue
 
@@ -254,7 +245,12 @@ class PathDocs(object):
         print(kkk)
 
 
-        def NeuronParc():
+
+
+
+
+
+       
 
 
 
@@ -281,14 +277,107 @@ if((input("Пройтись по таблице? 1-да 2-нет"))=="1"):
     PathDocs.CalcDifference()
 
 
-#pprint(ListPos)
 
 
-"""
-for i in range(len(ListPos)):
-    if Man in ListPos[i]:
-        print(ListPos[i])
-"""
+
+
+class NeuralNetwork():
+    def __init__(self):
+        # Запустить генератор случайных чисел, чтобы он генерировал те же числа
+        # каждый раз, когда программа запускается.
+
+        random.seed(1)
+
+        # Мы моделируем один нейрон с 3 входными и 1 выходным соединением.
+        # Мы присваиваем случайные веса матрице 3 x 1 со значениями в диапазоне от -1 до 1
+        # и означает 0.
+        self.synaptic_weights = 2 * random.random((14, 1)) - 1
+
+    # Сигмовидная функция, которая описывает S-образную кривую.
+    # Мы передаем взвешенную сумму входных данных через эту функцию
+    # нормализуйте их между 0 и 1.
+    def __sigmoid(self, x):
+        return 1 / (1 + exp(-x))
+
+
+    # Производная сигмоидной функции.
+    # Это градиент сигмовидной кривой.
+    # Это показывает, насколько мы уверены в существующем весе.
+    def __sigmoid_derivative(self, x):
+        return x * (1 - x)
+
+
+    # Мы обучаем нейронную сеть в процессе проб и ошибок.
+    # Регулировка синаптических весов каждый раз.
+    def train(self, training_set_inputs, training_set_outputs, number_of_training_iterations):
+        for iteration in range(number_of_training_iterations):
+            # Пройдите обучение через нашу нейронную сеть (один нейрон).
+            output = self.think(training_set_inputs)
+
+            # Рассчитать ошибку (разница между желаемым выводом
+            # и прогнозируемый результат).
+            error = training_set_outputs - output
+
+
+            # Умножьте ошибку на вход и снова на градиент сигмоидной кривой.
+            # Это означает, что менее уверенные веса корректируются больше.
+            # Это означает, что входные данные, которые равны нулю, не вызывают изменения весов.
+            adjustment = dot(training_set_inputs.T, error * self.__sigmoid_derivative(output))
+
+            
+            # Отрегулируйте вес.
+            self.synaptic_weights += adjustment
+
+
+    # Нейронная сеть думает.
+    def think(self, inputs):
+        
+        # Передача входных данных через нашу нейронную сеть (наш единственный нейрон).
+        return self.__sigmoid(dot(inputs, self.synaptic_weights))
+
+
+if __name__ == "__main__":
+
+    # Запуск одной нейронной сети нейронов.
+
+    neural_network = NeuralNetwork()
+
+    print ("Random starting synaptic weights: ")
+    print (neural_network.synaptic_weights)
+
+
+    # Учебный комплект. У нас есть 4 примера, каждый из которых состоит из 3 входных значений
+    # и 1 выходное значение.
+    #training_set_inputs = array([[0, 0, 1, 1, 1], [1, 1, 1, 1, 0], [1, 0, 1, 1, 1], [0, 1, 1, 1, 1]])
+    #training_set_outputs = array([[0, 1, 1, 0]]).T
+
+    with open('Text.txt', 'r') as file:
+        lst = file.readlines()
+    lst = [[int(n) for n in x.split()] for x in lst]
+    lstout=[]
+    for i in range(len(lst)):
+        lstout.append(lst[i][-1])
+        del lst[i][-1]
+    print(len(array(lstout)))
+    print(lstout)
+    print(len(array(lst)))
+    pprint(lst)
+
+    training_set_inputs = array(lst)
+    training_set_outputs = array([lstout]).T
+
+
+
+    # Тренируйте нейронную сеть, используя тренировочный набор.
+    # Сделайте это 10000 раз и вносите небольшие корректировки каждый раз.
+    neural_network.train(training_set_inputs, training_set_outputs, 10000)
+
+    print ("Новая таблица массы синапсов: ")
+    print (neural_network.synaptic_weights)
+
+    # Протестируйте нейронную сеть в новой ситуации.
+    print ("Моделируем новую ситуацию [1, 0, 0] -> ?: ")
+    print (neural_network.think(array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])))
 
 
 
